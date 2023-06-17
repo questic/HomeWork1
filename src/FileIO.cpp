a19d23d6df2dffc162613e329300505a9af487da
questic <questic1@gmail.com>
2023-06-17 23:05:00 +0300

    feat: add core file input/output logic

--- src/FileIO.cpp
+++ src/FileIO.cpp
@@ -1,1 +1,34 @@
 #include "FileIO.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+
+bool IsFileExist(std::string fileName) {
+    std::fstream file;
+
+    file.open(fileName);
+
+    return !file.fail() ? true : false;
+}
+
+void CreateFile(std::string fileName) {
+    std::ofstream file(fileName);
+    file.close();
+}
+
+bool IsFileEmpty(std::string fileName) {
+    std::ifstream file(fileName);
+
+    return file.peek() == EOF ? true : false;
+}
+
+void PrintFileData(std::string fileName) {
+    std::ifstream file(fileName);
+    std::string line;
+
+    while (!file.eof()) {
+        std::getline(file, line);
+        std::cout << line << std::endl;
+    }
+}
--- src/FileIO.h
+++ src/FileIO.h
@@ -1,1 +1,9 @@
 #pragma once
+#include <string>
+#include <sstream>
+
+
+bool IsFileExist(std::string fileName);
+void CreateFile(std::string fileName);
+bool IsFileEmpty(std::string fileName);
+void PrintFileData(std::string fileName);
